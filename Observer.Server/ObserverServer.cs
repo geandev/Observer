2162487a2be30601cfee59ef2c5ee9d833b5ab49
Gean Alexandre <geanalexandre@hotmail.com>
2018-06-06 14:25:59 -0300

    removed unsed mvc dependency

--- Observer.Server/ObserverServer.cs
+++ Observer.Server/ObserverServer.cs
@@ -1,13 +1,10 @@
 ﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Observer.Core.Client;
-using Observer.Core.Models;
 using Observer.Core.Server;
 using Observer.Core.Store;
 
 namespace Observer.Server
 {
-    [ApiController]
     public class ObserverServer : IObserverServer
     {
         private readonly IClientStore _clientStore;
@@ -17,10 +14,8 @@ namespace Observer.Server
             _clientStore = builder.Storage;
         }
 
-        [HttpPost(Endpoints.Connect)]
         public Task ConnectAsync<TObservable>(TObservable client) where TObservable : IObservableClient => _clientStore.SaveAsync(client);
 
-        [HttpPost(Endpoints.Disconnect)]
         public Task DisconnectAsync<TObservable>(TObservable client) where TObservable : IObservableClient => _clientStore.SaveAsync(client);
     }
 }
